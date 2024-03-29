faeb570ad71567586d1e3ed815085430a5d1ce6f
plainp <tarantoj04@gmail.com>
2024-03-29 15:41:14 -0300

    chore: First Inicialization

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
-  experimental: {
-    serverActions: true,
-  },
+  // experimental: {
+  //   serverActions: true,
+  // },
 };
 
 module.exports = nextConfig;
